c6bcfa84b4bb6b728f8343636cff60eab2fedda6
Mike <m.kostevich@mageworx.com>
2022-05-04 16:11:10 +0300

    dev

---
+++ Model/Resolver/RemoveRewardPointsFromCart.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Copyright © MageWorx. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace MageWorx\RewardPointsGraphQl\Model\Resolver;
+
+use Magento\Framework\Exception\CouldNotSaveException;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Exception\GraphQlAuthorizationException;
+use Magento\Framework\GraphQl\Exception\GraphQlInputException;
+use Magento\Framework\GraphQl\Exception\GraphQlNoSuchEntityException;
+use Magento\Framework\GraphQl\Query\Resolver\ContextInterface;
+use Magento\Framework\GraphQl\Query\Resolver\Value;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+use Magento\QuoteGraphQl\Model\Cart\GetCartForUser;
+use MageWorx\RewardPoints\Api\RewardManagerInterface;
+
+class RemoveRewardPointsFromCart implements ResolverInterface
+{
+    /**
+     * @var GetCartForUser
+     */
+    protected $getCartForUser;
+
+    /**
+     * @var RewardManagerInterface
+     */
+    protected $rewardManager;
+
+    /**
+     * @param GetCartForUser $getCartForUser
+     * @param RewardManagerInterface $rewardManager
+     */
+    public function __construct(GetCartForUser $getCartForUser, RewardManagerInterface $rewardManager)
+    {
+        $this->getCartForUser = $getCartForUser;
+        $this->rewardManager  = $rewardManager;
+    }
+
+    /**
+     * @param Field $field
+     * @param ContextInterface $context
+     * @param ResolveInfo $info
+     * @param array|null $value
+     * @param array|null $args
+     * @return Value|mixed
+     * @throws LocalizedException
+     */
+    public function resolve(Field $field, $context, ResolveInfo $info, array $value = null, array $args = null)
+    {
+        if (empty($args['input']['cart_id'])) {
+            throw new GraphQlInputException(__('Required parameter "cart_id" is missing'));
+        }
+
+        if ($context->getExtensionAttributes()->getIsCustomer() === false) {
+            throw new GraphQlAuthorizationException(
+                __('The current customer isn\'t authorized.')
+            );
+        }
+
+        $maskedCartId  = $args['input']['cart_id'];
+        $currentUserId = $context->getUserId();
+        $storeId       = (int)$context->getExtensionAttributes()->getStore()->getId();
+        $cart          = $this->getCartForUser->execute($maskedCartId, $currentUserId, $storeId);
+        $cartId        = (int)$cart->getId();
+
+        try {
+            $this->rewardManager->remove($cartId);
+        } catch (NoSuchEntityException $e) {
+            throw new GraphQlNoSuchEntityException(__($e->getMessage()), $e);
+        } catch (CouldNotSaveException $e) {
+            throw new LocalizedException(__($e->getMessage()), $e);
+        }
+
+        return [
+            'cart' => [
+                'model' => $cart,
+            ],
+        ];
+    }
+}
